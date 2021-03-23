7337a736459e2826d7c5f9850b2b8b0164948752
Sudharshan Atreya <sudharshan.atreya@fmc-na.com>
2021-03-23 19:53:48 +0530

    Initial setup

---
+++ template-builder-server/src/main/groovy/com/test/templatebuilderserver/TemplateBuilderServerApplication.groovy
@@ -0,0 +1,13 @@
+package com.test.templatebuilderserver
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TemplateBuilderServerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TemplateBuilderServerApplication, args)
+	}
+
+}
---
+++ template-builder-server/src/test/groovy/com/test/templatebuilderserver/TemplateBuilderServerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.test.templatebuilderserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemplateBuilderServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
