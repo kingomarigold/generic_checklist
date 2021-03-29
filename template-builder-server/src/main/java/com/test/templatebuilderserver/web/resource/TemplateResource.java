9f603fa0bbc6481cbdbf07838255f0c6b1ee5386
Karthik Subramanian Ramnath <kingomarigold@yahoo.com>
2021-03-29 18:11:29 +0530

    Added CORS headers to the server

--- template-builder-server/src/main/java/com/test/templatebuilderserver/web/resource/TemplateResource.java
+++ template-builder-server/src/main/java/com/test/templatebuilderserver/web/resource/TemplateResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.test.templatebuilderserver.dto.Template;
 import com.test.templatebuilderserver.service.TemplateService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class TemplateResource {
 
